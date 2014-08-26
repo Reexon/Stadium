b51f3c879b4614a89fc44ca7e126b358ac26a37d
Loris <loris@reexon.net>
2014-08-27 01:02:55 +0200

    Visualizzazione Utenti
    * Visualizzazione utenti
    * Eliminazione Utenti

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -12,6 +12,41 @@
 
     @section('content')
 
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr>
+        <td>ID</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>Email</td>
+        <td>Register Date</td>
+        <td>Actions</td>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($users as $user)
+    <tr>
+        <td>{{ $user->id_user }}    </td>
+        <td>{{ $user->firstname }}  </td>
+        <td>{{ $user->lastname }}   </td>
+        <td>{{ $user->email }}      </td>
+        <td>{{ $user->created_at }} </td>
 
-None
+        <td>
+            <!-- GET /nerds/{id} -->
+            <a class="btn btn-small btn-success" href="{{ URL::to('users/' . $user->id_user) }}">{{ FA::icon('eye'); }}</a>
+
+            <!-- GET /nerds/{id}/edit -->
+            <a class="btn btn-small btn-info" href="{{ URL::to('users/' . $user->id_user . '/edit') }}">{{ FA::icon('pencil'); }}</a>
+
+            <!-- TODO: Confirmation Before Delete -->
+            {{ Form::open(array('url' => 'users/' . $user->id_user,'style' =>'display:inline-block')) }}
+            {{ Form::hidden('_method', 'DELETE') }}
+            {{ Form::button(FA::icon('trash-o'), array('type' => 'submit', 'class' => 'btn btn-small btn-danger'))}}
+            {{ Form::close() }}
+        </td>
+    </tr>
+    @endforeach
+    </tbody>
+</table>
     @stop
