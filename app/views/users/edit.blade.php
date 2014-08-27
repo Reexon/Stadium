4f51af2cb441f7274f0b95b123454151a1fa1ef9
Loris <loris@reexon.net>
2014-08-27 17:13:51 +0200

    User Edit Layout
    * Inserito Layout Per l’editing di un user

--- app/views/users/edit.blade.php
+++ app/views/users/edit.blade.php
@@ -1,15 +1,47 @@
 @extends('layouts.master')
 
-    @section('head')
-        @parent
-    @stop
+@section('head')
+@parent
+@stop
 
 
-    @section('navigation')
-        @parent
-    @stop
+@section('navigation')
+@parent
+@stop
 
 
-    @section('content')
+@section('content')
 
-    @stop
+<h1>Edit {{ $user->firstname }} {{ $user->lastname}}</h1>
+
+
+
+{{ Form::model($user, array('route' => array('users.update', $user->id_user), 'method' => 'PUT','class'=>'form-horizontal', 'role' => 'form')) }}
+
+<div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">First Name</label>
+    <div class="col-sm-10">
+        {{ Form::text('firstname', Input::old('firstname'), array('class' => 'form-control')) }}
+    </div>
+</div>
+<div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Last Name</label>
+    <div class="col-sm-10">
+        {{ Form::text('lastname', Input::old('lastname'), array('class' => 'form-control')) }}
+    </div>
+</div>
+<div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+        {{ Form::text('email', Input::old('email'), array('class' => 'form-control')) }}
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+        {{ Form::submit('Edit User!', array('class' => 'btn btn-primary')) }}
+    </div>
+</div>
+{{ Form::close() }}
+
+@stop
