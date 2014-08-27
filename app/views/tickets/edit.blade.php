d3fd6731841e755eef337925bc341ad1f7da922b
Loris <loris@reexon.net>
2014-08-28 00:34:55 +0200

    Edit Ticket
    * Pagina Modifica Ticket

--- app/views/tickets/edit.blade.php
+++ app/views/tickets/edit.blade.php
@@ -1,1 +1,54 @@
-/Applications/AMPPS/www/Stadium/app/views/tickets/edit.blade.php
+@extends('layouts.master')
+
+@section('head')
+@parent
+@stop
+
+
+@section('navigation')
+@parent
+@stop
+
+
+@section('content')
+
+<h1>{{ $ticket->match->home_team}} vs {{ $ticket->match->guest_team }} ({{ $ticket->label }}) - {{ $ticket->match->date->format('d.m.Y') }}</h1>
+
+{{ Form::model($ticket, array('route' => array('tickets.update', $ticket->id_ticket), 'method' => 'PUT','class'=>'form-horizontal', 'role' => 'form')) }}
+
+<div class="form-group">
+    {{ Form::label('label','Ticket Type',['class' => 'col-sm-2 control-label']) }}
+    <div class="col-sm-10">
+        {{ Form::text('label', Input::old('label'), array('class' => 'form-control')) }}
+    </div>
+</div>
+
+<div class="form-group">
+    {{ Form::label('price','Price',['class' => 'col-sm-2 control-label']) }}
+    <div class="col-sm-10">
+        {{ Form::text('price', Input::old('price'), array('class' => 'form-control')) }}
+    </div>
+</div>
+
+<div class="form-group">
+    {{ Form::label('quantity','Quantity',['class' => 'col-sm-2 control-label']) }}
+    <div class="col-sm-10">
+        {{ Form::text('quantity', Input::old('quantity'), array('class' => 'form-control')) }}
+    </div>
+</div>
+
+<div class="form-group">
+    {{ Form::label('match_id','Match',['class' => 'col-sm-2 control-label']) }}
+    <div class="col-sm-10">
+        {{ Form::select('match_id', $matches,$ticket->match->id_match ) }}
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+        {{ Form::submit('Edit Ticket!', array('class' => 'btn btn-primary')) }}
+    </div>
+</div>
+{{ Form::close() }}
+
+@stop
