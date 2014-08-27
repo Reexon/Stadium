e1f9c8f6cb35273e521bf7d6e1a4f07f23182354
Loris <loris@reexon.net>
2014-08-27 17:37:03 +0200

    Layout Blade matches.show.blade
    Eliminato il Raw HTML e Inserito template Blade

--- app/views/matches/show.blade.php
+++ app/views/matches/show.blade.php
@@ -1,34 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{ $match->home_team }} vs {{ $match->guest_team }}</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
+@extends('layouts.master')
 
-    <!-- da globalizzare -->
-    <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="{{ URL::to('matches') }}">Nerd Alert</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li><a href="{{ URL::to('matches') }}">View All Nerds</a></li>
-            <li><a href="{{ URL::to('matches/create') }}">Create a Nerd</a>
-        </ul>
-    </nav>
-    <!-- da globalizzare -->
+@section('head')
+@parent
+@stop
+
+
+@section('navigation')
+@parent
+@stop
+
+
+@section('content')
 
     <h1>Showing {{ $match->home_team }} vs {{ $match->guest_team }}</h1>
 
     <div class="jumbotron text-center">
-        <h2>{{ $match->home_team }} vs {{ $match->guest_team }}</h2>
         <p>
-            <strong>Date:</strong> {{ $match->date }}<br>
+            <strong>Date:</strong> {{ $match->date->format('m-d-Y') }}<br>
             <strong>Ticket:</strong>
         </p>
     </div>
 
-</div>
-</body>
-</html>
+@stop
