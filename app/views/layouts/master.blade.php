0b84c8ae22467d6839d95c2a549ef535f559520a
Loris <loris@reexon.net>
2014-08-29 12:58:02 +0200

    Aggiunto Menu Payment

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -60,6 +60,18 @@
                 </ul>
             </li>
 
+
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{FA::icon('user')}}  Payment <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="{{ URL::to('payments') }}">{{FA::icon('eye')}}  Show All Payment</a></li>
+                    <li><a href="{{ URL::to('payments/create') }}">{{FA::icon('plus')}}  Add Payment</a></li>
+                    <li><a href="#">More 1</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">More 2</a></li>
+                </ul>
+            </li>
+
         </ul>
         <ul class="nav navbar-nav navbar-right">
             <li>
