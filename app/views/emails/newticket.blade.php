94a817a2a5085077299a9f6d6e86e7b9494e7154
Loris <loris@reexon.net>
2014-09-08 23:05:36 +0200

    template mail creazione nuovo ticket

--- app/views/emails/newticket.blade.php
+++ app/views/emails/newticket.blade.php
@@ -1,5 +1,10 @@
-thank you for your subscribe !
+<?php $match = unserialize($match); ?>
 
-ticket: {{ $ticket_id }}
+New Ticket Are avvailable for match <b>{{$match->homeTeam->name}} vs {{$match->guestTeam->name}}</b> ({{$match->date->format('d.m.Y')}})
+<br/>
+<a href="{{ URL::to('match/info/'.$match->id_match) }}">You can show and buy the ticket directly from here</a>
+<br/>
+<br/>
 
-<a href="{{ URL::to('match/info/'.$ticket_id) }}">LINK</a>
+This email was sent to you, because you've subscribed to current match.<br/>
+Stadium Srl.
